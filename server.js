14510ad50139bb3c3b963c2b3f63a7256fce2855
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-10-31 06:04:53 +0200

    Create server.js
    Update server.js
    
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Porta dinâmica (usada pelo Render)
+const PORT = process.env.PORT || 10000;
+
+// Servir arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Rota principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
