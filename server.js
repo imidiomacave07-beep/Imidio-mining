8dc4227d738dd247d3b766599ece4fc86edf2cb9
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-11-06 13:20:46 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 10000;  // pega a porta do Render ou usa 10000 localmente
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
