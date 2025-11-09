128deb6cc5ad30026204c220e7172bb8f182476c
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-11-09 06:05:30 +0200

    Update server.js
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+// server.js - plataforma Imidio Mining
+
 const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve arquivos estáticos da pasta "public"
+// Middleware para servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota principal
+// Rota principal (para o index.html)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Rota para a página home (opcional)
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
 // Porta (Render define automaticamente)
