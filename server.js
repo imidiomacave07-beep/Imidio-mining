094566d5c9c85b970d7bd02e8e01c51d6bbd420f
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-11-13 13:17:59 +0200

    Refactor server.js for consistent quotation marks
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-// server.js
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
-
+const express = require("express");
+const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 10000;
 
-// Servir arquivos estáticos da pasta 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
