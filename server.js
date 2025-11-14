690aca9caefb8848931457b8f965f19e05d3c11f
imidiomacave07-beep <imidiomacave07@gmail.com>
2025-11-14 04:29:17 +0200

    Fix duplicate server listen statement
    Signed-off-by: imidiomacave07-beep <imidiomacave07@gmail.com>

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// Servir arquivos estáticos da pasta public
 app.use(express.static(path.join(__dirname, "public")));
 
+// Rota principal
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Iniciar o servidor
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
